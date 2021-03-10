0442c9358a63d49131628674b01f5fcd2511ba67
Jiubei0408 <757795210@qq.com>
2021-03-11 00:20:12 +0800

    升级springboot, 解决跨域问题

---
+++ src/main/java/top/kealine/zuccoj/config/SpringSessionConfig.java
@@ -0,0 +1,20 @@
+package top.kealine.zuccoj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class SpringSessionConfig {
+    public SpringSessionConfig() {
+    }
+
+    @Bean
+    public CookieSerializer httpSessionIdResolver() {
+        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+        // 取消仅限同一站点设置
+        cookieSerializer.setSameSite(null);
+        return cookieSerializer;
+    }
+}
